3f4b6f3b20cc583a31ba5d642778c83c0d60da21
Sosumiii <146884277+Sosumiii@users.noreply.github.com>
2024-12-16 21:29:17 -0500

    Add files via upload

---
+++ 241Review/246PokemonBattleSim/MainPokemonBattle.java
@@ -0,0 +1,19 @@
+/*
+ * Project 2.4.6 Elvis Nguyen
+ * Program: Pokemon Battle Simulation
+ */
+
+
+public class 246PokemonBattle
+{
+    public static void main(String[] args)
+    {
+        int pokemonHealth = 100;
+        int playerPokemonHealth = 100;
+
+        while (pokemonHealth >= 0 || playerPokemonHealth >= 0)
+        {
+            
+        }
+    }
+}
---
+++ 241Review/246PokemonBattleSim/PokemonBattleSubsystem.java
@@ -0,0 +1,20 @@
+/*
+ * Project 2.4.6 Elvis Nguyen
+ * Pokemon Battle Subsystem
+ */
+
+public class PokemonBattleSubsystem
+public static int useItem(String item)
+{
+    int potionValue = 15;
+    int strongPotionValue = 25;
+    int potionAmt = 4;
+    int strongPotionAmt = 2;
+    int antidoteAmt = 3;
+
+    boolean antidote = false;
+
+    if (item = "potion" || item = "Strong Potion")
+
+        
+}
