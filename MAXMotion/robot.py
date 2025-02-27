7b6e1ee7246367b31a95c869219f6a97ce4eeaef
Sosumiii <146884277+Sosumiii@users.noreply.github.com>
2025-02-27 15:21:23 -0500

    Update robot.py

--- MAXMotion/robot.py
+++ MAXMotion/robot.py
@@ -12,24 +12,35 @@ class MyRobot(wpilib.TimedRobot):
         motorConfig = rev.SparkMaxConfig()
         
         motorConfig.setIdleMode(rev.SparkMaxConfig.IdleMode.kBrake)
-        motorConfig.smartCurrentLimit(10)
+        motorConfig.smartCurrentLimit(40)
         
         PIDConfig = motorConfig.closedLoop
-        PIDConfig.pid(0.001, 0.0, 0.0, rev.ClosedLoopSlot.kSlot0)
+        PIDConfig.pid(0.0004, 0.0, 0.0, rev.ClosedLoopSlot.kSlot0)
         PIDConfig.outputRange(-1, 1)
         PIDConfig.setFeedbackSensor(rev.SparkMaxConfig().closedLoop.FeedbackSensor.kPrimaryEncoder)
                 
         MAXMotion = motorConfig.closedLoop.maxMotion
-        MAXMotion.maxAcceleration(5, rev.ClosedLoopSlot.kSlot0)
-        MAXMotion.maxVelocity(5, rev.ClosedLoopSlot.kSlot0)
-        MAXMotion.allowedClosedLoopError(1, rev.ClosedLoopSlot.kSlot0)
+        MAXMotion.maxAcceleration(10000, rev.ClosedLoopSlot.kSlot0)
+        MAXMotion.maxVelocity(10000, rev.ClosedLoopSlot.kSlot0)
+        #MAXMotion.allowedClosedLoopError(0.5, rev.ClosedLoopSlot.kSlot0)
         MAXMotion.MAXMotionPositionMode.kMAXMotionTrapezoidal
         
-        self.motor.configure(motorConfig, self.motor.ResetMode.kResetSafeParameters, self.motor.PersistMode.kNoPersistParameters)
+        self.motor.configure(motorConfig, self.motor.ResetMode.kResetSafeParameters, self.motor.PersistMode.kPersistParameters)
         
         
         return super().robotInit()
     
+    def robotPeriodic(self):
+        wpilib.SmartDashboard.putNumber("Encoder Value", self.encoder.getPosition())
+        return super().robotPeriodic()
+    
     def testPeriodic(self) -> None:
         self.closedLoopController.setReference(42, self.motor.ControlType.kMAXMotionPositionControl, rev.ClosedLoopSlot.kSlot0)
+        #self.motor.set(cmd)
+
+        
         return super().testPeriodic()
+    
+    def teleopPeriodic(self):
+        self.closedLoopController.setReference(0, self.motor.ControlType.kMAXMotionPositionControl, rev.ClosedLoopSlot.kSlot0)
+        return super().teleopPeriodic()
