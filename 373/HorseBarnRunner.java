bbb24cb9ba9f0449ed9fa174f4319fa6336abb43
Sosumiii <146884277+Sosumiii@users.noreply.github.com>
2025-03-11 23:26:39 -0400

    373 complete

--- 373/HorseBarnRunner.java
+++ 373/HorseBarnRunner.java
@@ -13,21 +13,44 @@ public class HorseBarnRunner
     ArrayList<Horse> barnSpaces = barn.getSpaces();
 
     int numSpaces = barnSpaces.size();
-    for (int i = 0; i < numSpaces - 1; i++)
+    /* for (int i = 0; i < numSpaces - 1; i++)
     {
       Horse h = barnSpaces.get(i);
       if (h.getName().equals("Patches"))
       {
         System.out.println("Bye bye " +  barnSpaces.remove(i));
         i--;
+        numSpaces--;
       }
 
       else if (h.getName().equals("Lady"))
       {
         System.out.println("Bye bye " + barnSpaces.remove(i));
         i--;
+        numSpaces--;
       }
+    } */
+    int i = 0;
+    while (i < numSpaces)
+    {
+      Horse h = barnSpaces.get(i);
+      if (h.getName().equals("Patches"))
+      {
+        System.out.println("Bye bye " +  barnSpaces.remove(i));
+        i--;
+        numSpaces--;
+      }
+
+      else if (h.getName().equals("Lady"))
+      {
+        System.out.println("Bye bye " + barnSpaces.remove(i));
+        i--;
+        numSpaces--;
+      }
+      i++;
     }
+
+    System.out.println("");
     for (Horse horse : barnSpaces)
     {
       System.out.println(horse);
